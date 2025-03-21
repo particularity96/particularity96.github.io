3467b4c663e651cc9d7a48f8a9a2545114142c01
Nic Schilling <niclas.schilling96@gmail.com>
2025-03-21 16:10:01 +0100

    feat: setup react in sub folder

---
+++ website/src/App.tsx
@@ -0,0 +1,12 @@
+import MyComponent from './Header';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Welcome to my app!</h1>
+            <MyComponent message="Hello from my component!" />
+        </div>
+    );
+}
+
+export default App;
