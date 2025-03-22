09876e3cc3ae7954b100ea0ded6d866987602685
Nic Schilling <niclas.schilling96@gmail.com>
2025-03-22 15:37:01 +0100

    feat: switch from html to react

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import MyComponent from './Header';
 function App() {
     return (
         <div className="App">
-            <h1>Welcome to my app!</h1>
-            <MyComponent message="Hello from my component!" />
+            <h1>Nic Schilling</h1>
+            <MyComponent message="This page is currently under construction!" />
         </div>
     );
 }
