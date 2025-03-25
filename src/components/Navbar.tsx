ab50c550d41fe73d38f4100e58d185e52cd5d421
Nic Schilling <niclas.schilling96@gmail.com>
2025-03-25 12:26:44 +0100

    Make mobile sliding navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,28 @@
-import { Link } from "react-router-dom";
+﻿import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
         <nav className="navbar">
             <div className="navbar-container">
                 <h1 className="logo">Nic Schilling</h1>
-                <ul className="nav-links">
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/choir">Chor</Link></li>
-                    <li><Link to="/musical">Musical</Link></li>
-                    <li><Link to="/games">Games</Link></li>
-                    <li><Link to="/about">Vita</Link></li>
-                    <li><Link to="/archive">Archiv</Link></li>
-                    <li><Link to="/contact">Kontakt</Link></li>
+
+                {/* Mobile Menu Button */}
+                <button className="menu-button" onClick={() => setMenuOpen(!menuOpen)}>
+                    ☰
+                </button>
+
+                {/* Desktop Navigation (Visible on PC) */}
+                <ul className={`nav-links ${menuOpen ? "mobile-active" : ""}`}>
+                    <li><Link to="/" onClick={() => setMenuOpen(false)}>Home</Link></li>
+                    <li><Link to="/choir" onClick={() => setMenuOpen(false)}>Chor</Link></li>
+                    <li><Link to="/musical" onClick={() => setMenuOpen(false)}>Musical</Link></li>
+                    <li><Link to="/games" onClick={() => setMenuOpen(false)}>Games</Link></li>
+                    <li><Link to="/about" onClick={() => setMenuOpen(false)}>Vita</Link></li>
+                    <li><Link to="/archive" onClick={() => setMenuOpen(false)}>Archiv</Link></li>
+                    <li><Link to="/contact" onClick={() => setMenuOpen(false)}>Kontakt</Link></li>
                 </ul>
             </div>
         </nav>
