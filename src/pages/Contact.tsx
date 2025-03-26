d543ac90c6bd83d37de4b41b5c73d0115f717be6
Nic Schilling <niclas.schilling96@gmail.com>
2025-03-26 14:34:18 +0100

    Adjust min height of contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -6,29 +6,31 @@ import { Link } from "react-router-dom";
 
 const Contact: React.FC = () => {
     return (
-        <div className="contact-container">
-            <h1>Kontakt</h1>
-            <ul className="contact-list">
-                <li><strong>Email:</strong> nic.schilling96@gmail.com</li>
-            </ul>
-            <div className="social-buttons">
-                <a href="https://www.instagram.com/nic.schilling/" target="_blank" rel="noopener noreferrer" className="social-button">
-                    <img src={instagramIcon} alt="Instagram" />
-                </a>
-                <a href="https://www.linkedin.com/in/nic-schilling-90828a19a/" target="_blank" rel="noopener noreferrer" className="social-button">
-                    <img src={linkedinIcon} alt="LinkedIn" />
-                </a>
-                <a href="https://www.youtube.com/channel/UCqIDHVXrbbAt0cZ1OLlyZBw" target="_blank" rel="noopener noreferrer" className="social-button">
-                    <img src={youtubeIcon} alt="YouTube" />
-                </a>
-            </div>
-            <div className="legal-links">
-                <ul>
-                    <li><Link to="/impressum">Impressum</Link></li> {/* Use Link component */}
-                    <li><Link to="/datenschutz">Datenschutz</Link></li> {/* Use Link component */}
+        <div className="fullScreenContainer">
+            <div className="contact-container">
+                <h1>Kontakt</h1>
+                <ul className="contact-list">
+                    <li><strong>Email:</strong> nic.schilling96@gmail.com</li>
                 </ul>
+                <div className="social-buttons">
+                    <a href="https://www.instagram.com/nic.schilling/" target="_blank" rel="noopener noreferrer" className="social-button">
+                        <img src={instagramIcon} alt="Instagram" />
+                    </a>
+                    <a href="https://www.linkedin.com/in/nic-schilling-90828a19a/" target="_blank" rel="noopener noreferrer" className="social-button">
+                        <img src={linkedinIcon} alt="LinkedIn" />
+                    </a>
+                    <a href="https://www.youtube.com/channel/UCqIDHVXrbbAt0cZ1OLlyZBw" target="_blank" rel="noopener noreferrer" className="social-button">
+                        <img src={youtubeIcon} alt="YouTube" />
+                    </a>
+                </div>
+                <div className="legal-links">
+                    <ul>
+                        <li><Link to="/impressum">Impressum</Link></li> {/* Use Link component */}
+                        <li><Link to="/datenschutz">Datenschutz</Link></li> {/* Use Link component */}
+                    </ul>
+                </div>
             </div>
-        </div>
+            </div >
     );
 };
 
