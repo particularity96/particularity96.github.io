28836a97af46742b2e9650561aca2372167f3013
Nic Schilling <niclas.schilling96@gmail.com>
2025-03-27 01:50:30 +0100

    React Helmet

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import {
     Route,
     Link
 } from "react-router-dom";
+import { Helmet } from 'react-helmet'; 
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Choir from "./pages/Choir";
@@ -14,7 +15,6 @@ import Archive from "./pages/Archive";
 import Contact from "./pages/Contact";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Animated from "./components/Animated"; // Import the new animated component
 import Impressum from "./pages/Impressum";
 import Datenschutz from "./pages/Datenschutz";
 
@@ -23,16 +23,87 @@ const App: React.FC = () => {
         <Router>
             <Navbar />
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/choir" element={<Choir />} />
-                <Route path="/musical" element={<Musical />} />
-                <Route path="/games" element={<Games />} />
-                <Route path="/archive" element={<Archive />} />
-                <Route path="/contact" element={<Contact />} />
-                <Route path="/animated" element={<Animated />} /> 
-                <Route path="/impressum" element={<Impressum />} />
-                <Route path="/datenschutz" element={<Datenschutz />} />
+                <Route path="/" element={
+                    <>
+                        <Helmet>
+                            <title>Home | Nic Schilling</title>
+                            <meta name="description" content="Musik - Theater - Chor" /> <meta name="robots" content="index, follow" />
+                        </Helmet>
+                        <Home />
+                    </>
+                } />
+                <Route path="/about" element={
+                    <>
+                        <Helmet>
+                            <title>Vita | Nic Schilling</title>
+                            <meta name="description" content="Eine Kurzbiografie von Nic Schilling." />
+                        </Helmet>
+                        <About />
+                    </>
+                } />
+                <Route path="/choir" element={
+                    <>
+                        <Helmet>
+                            <title>Chor | Nic Schilling</title>
+                            <meta name="description" content="Kompositionen, Chorwerke und Vokal-Arrangements von Nic Schilling mit Hörbeispielen." />
+                        </Helmet>
+                        <Choir />
+                    </>
+                } />
+                <Route path="/musical" element={
+                    <>
+                        <Helmet>
+                            <title>Musical | Nic Schilling</title>
+                            <meta name="description" content="Musical, Musiktheater und Live-Hörspiele von Nic Schilling mit Hörbeispielen." />
+                        </Helmet>
+                        <Musical />
+                    </>
+                } />
+                <Route path="/games" element={
+                    <>
+                        <Helmet>
+                            <title>Games | Nic Schilling</title>
+                            <meta name="description" content="Interaktive Bildung, Extended Reality und Games!" />
+                        </Helmet>
+                        <Games />
+                    </>
+                } />
+                <Route path="/archive" element={
+                    <>
+                        <Helmet>
+                            <title>Archiv | Nic Schilling</title>
+                            <meta name="description" content="Eine Sammlung aller wesentlichen Projekte von und mit Nic Schilling." />
+                        </Helmet>
+                        <Archive />
+                    </>
+                } />
+                <Route path="/contact" element={
+                    <>
+                        <Helmet>
+                            <title>Kontakt | Nic Schilling</title>
+                            <meta name="description" content="Kontaktmöglichkeiten für Anfragen und Aufträge." />
+                        </Helmet>
+                        <Contact />
+                    </>
+                } />
+                <Route path="/impressum" element={
+                    <>
+                        <Helmet>
+                            <title>Impressum | Nic Schilling</title>
+                            <meta name="description" content="Rechtlich verbindliche Erklärung zur Verantwortung über den Inhalt" />
+                        </Helmet>
+                        <Impressum />
+                    </>
+                } />
+                <Route path="/datenschutz" element={
+                    <>
+                        <Helmet>
+                            <title>Datenschutz | Nic Schilling</title>
+                            <meta name="description" content="Erklärung zum Datenschutz." />
+                        </Helmet>
+                        <Datenschutz />
+                    </>
+                } />
             </Routes>
             <Footer />
         </Router>
